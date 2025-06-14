064e2c2b86984d7e4567c178274b01bfbd9bddc6
Rafen19 <noboarabel@gmail.com>
2025-06-14 16:15:42 +0000

    Implementamos vistas de configuracion

--- src/front/pages/configuracion/ConfigAdmin.jsx
+++ src/front/pages/configuracion/ConfigAdmin.jsx
@@ -2,8 +2,7 @@ import React from "react";
 import { DatosPersonales } from "./Datos/DatosPersonales.jsx";
 import { CambiarContrasena } from "./Datos/CambiarContrasena.jsx";
 import { MonedaPrincipal } from "./Datos/MonedaPrincipal.jsx";
-import { Personalizacion } from "./Datos/Personalizacion.jsx";
-import { AjustesAvanzados } from "./Datos/AjustesAvanzados.jsx";
+
 
 export const ConfigAdmin = () => {
   return (
@@ -15,10 +14,6 @@ export const ConfigAdmin = () => {
       <CambiarContrasena />
       <hr />
       <MonedaPrincipal />
-      <hr />
-      <Personalizacion />
-      <hr />
-      <AjustesAvanzados />
     </div>
   );
 };
